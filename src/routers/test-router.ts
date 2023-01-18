775c0aa575b812eb15deef443646b72463bb4e72
Renat <jeingo20@gmail.com>
2023-01-18 17:53:43 +0300

    add testController with class

--- src/routers/test-router.ts
+++ src/routers/test-router.ts
@@ -1,10 +1,6 @@
-import { Router, Request, Response } from 'express'
-import { HTTP_STATUSES } from '../constats/status'
-import { testService } from '../domain/test-service'
+import {Router} from 'express'
+import {testController} from "../controllers/test-controller";
 
 export const testRouter = Router({})
 
-testRouter.delete('/', async (req: Request, res: Response) => {
-    await testService.deleteAllDB()
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-})
+testRouter.delete('/', testController.clearAllCollection)

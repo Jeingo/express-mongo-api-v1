bf63e8bae8991d4038b75d9d11d98acac5e2055c
Renat <jeingo20@gmail.com>
2023-01-18 17:51:38 +0300

    add securityController with class

---
+++ src/controllers/security-controller.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from 'express'
+import { checkAuthorizationAndGetPayload } from '../routers/helper'
+import { HTTP_STATUSES } from '../constats/status'
+import { sessionsService } from '../domain/sessions-service'
+import { RequestWithParams } from '../models/types'
+import { DeviceIdParams } from '../models/auth-models'
+
+class SecurityController {
+    async getAllActiveSession(req: Request, res: Response) {
+        const gotRefreshToken = req.cookies.refreshToken
+
+        const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
+        if (!payload) {
+            res.clearCookie('refreshToken')
+            res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+            return
+        }
+
+        const allSession = await sessionsService.findAllActiveSession(payload.userId)
+        res.json(allSession)
+    }
+    async deleteAllSessionWithoutCurrent(req: Request, res: Response) {
+        const gotRefreshToken = req.cookies.refreshToken
+
+        const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
+        if (!payload) {
+            res.clearCookie('refreshToken')
+            res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+            return
+        }
+        await sessionsService.deleteActiveSessionWithoutCurrent(payload.userId, payload.iat)
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+    }
+    async deleteSessionById(req: RequestWithParams<DeviceIdParams>, res: Response) {
+        const gotRefreshToken = req.cookies.refreshToken
+
+        const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
+        if (!payload) {
+            res.clearCookie('refreshToken')
+            res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
+            return
+        }
+
+        const deletedDevice = await sessionsService.deleteSessionByDeviceId(
+            req.params.id,
+            payload.userId
+        )
+        if (deletedDevice === HTTP_STATUSES.NOT_FOUND_404) {
+            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+            return
+        }
+
+        if (deletedDevice === HTTP_STATUSES.FORBIDDEN_403) {
+            res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
+            return
+        }
+
+        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
+    }
+}
+
+export const securityController = new SecurityController()
--- src/routers/security-router.ts
+++ src/routers/security-router.ts
@@ -1,65 +1,10 @@
-import { Router, Request, Response } from 'express'
-import { HTTP_STATUSES } from '../constats/status'
-import { sessionsService } from '../domain/sessions-service'
-import { RequestWithParams } from '../models/types'
-import { DeviceIdParams } from '../models/auth-models'
-import { checkAuthorizationAndGetPayload } from './helper'
+import { Router } from 'express'
+import { securityController } from '../controllers/security-controller'
 
 export const securityRouter = Router({})
 
-securityRouter.get('/devices', async (req: Request, res: Response) => {
-    const gotRefreshToken = req.cookies.refreshToken
+securityRouter.get('/devices', securityController.getAllActiveSession)
 
-    const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
-    if (!payload) {
-        res.clearCookie('refreshToken')
-        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-        return
-    }
+securityRouter.delete('/devices', securityController.deleteAllSessionWithoutCurrent)
 
-    const allSession = await sessionsService.findAllActiveSession(payload.userId)
-    res.json(allSession)
-})
-
-securityRouter.delete('/devices', async (req: Request, res: Response) => {
-    const gotRefreshToken = req.cookies.refreshToken
-
-    const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
-    if (!payload) {
-        res.clearCookie('refreshToken')
-        res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-        return
-    }
-    await sessionsService.deleteActiveSessionWithoutCurrent(payload.userId, payload.iat)
-    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-})
-
-securityRouter.delete(
-    '/devices/:id',
-    async (req: RequestWithParams<DeviceIdParams>, res: Response) => {
-        const gotRefreshToken = req.cookies.refreshToken
-
-        const payload = await checkAuthorizationAndGetPayload(gotRefreshToken)
-        if (!payload) {
-            res.clearCookie('refreshToken')
-            res.sendStatus(HTTP_STATUSES.UNAUTHORIZED_401)
-            return
-        }
-
-        const deletedDevice = await sessionsService.deleteSessionByDeviceId(
-            req.params.id,
-            payload.userId
-        )
-        if (deletedDevice === HTTP_STATUSES.NOT_FOUND_404) {
-            res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
-            return
-        }
-
-        if (deletedDevice === HTTP_STATUSES.FORBIDDEN_403) {
-            res.sendStatus(HTTP_STATUSES.FORBIDDEN_403)
-            return
-        }
-
-        res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
-    }
-)
+securityRouter.delete('/devices/:id', securityController.deleteSessionById)
